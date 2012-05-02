ab3a5a4c1cc11683b5935c39df07aaa81550c025
Cyril Leroux <cyril.leroux@cgi.com>
2012-05-02 14:14:11 +0200

    Add Welcome function to AI

--- AI.cpp
+++ AI.cpp
@@ -1,7 +1,13 @@
 #include "AI.h"
+#include "Speech.h"
 
-AI::AI(void)
+AI::AI(string _name)
 {
+	m_Name = _name;
+	m_IsMute = false;
+	time(&m_LastWelcome);
+	m_LastWelcome -= 60 * 10; // - 10 minutes
+
 }
 
 AI::~AI(void)
@@ -10,9 +16,22 @@ AI::~AI(void)
 
 void AI::Welcome()
 {
+	// Welcome if last welcom > 10 min
+	
+	// Calcule du temps écoulé en minutes depuis le dernier appel à la fonction
+	time_t now;
+	time(&now);
+	double elapsedTime = difftime(m_LastWelcome, now) / 60.0;
+	if (elapsedTime > 10) {
+		Say("Welcome to " + m_Name + " sir." );
+		time(&m_LastWelcome);
+	}
 }
 
 void AI::Say(string _textToSpeak)
 {
 	// Test mute
+	if (!m_IsMute) {
+		Speech::Say(_textToSpeak);
+	}
 }
--- AI.h
+++ AI.h
@@ -1,16 +1,20 @@
 #pragma once
 
 #include <string>
+#include <time.h>
 using namespace std;
 
 class AI
 {
 private:
+	string m_Name;
 	int m_IsMute;
-	int m_LastWelcome;
+	time_t m_LastWelcome;
+
+	//double ElapsedTime(clock_t _clock1, clock_t _clock2);
 
 public:
-	AI(void);
+	AI(string _name);
 	~AI(void);
 	void Welcome();
 	void Say(string _textToSpeak);
