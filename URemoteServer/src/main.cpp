67a229d6dd7fefce4a991fbc4937544a8da51720
Cyril Leroux <cyril.leroux@cgi.com>
2013-01-18 17:14:35 +0100

    Working on exception handling

--- URemoteServer/src/main.cpp
+++ URemoteServer/src/main.cpp
@@ -9,53 +9,91 @@
 
 using namespace std;
 
-bool initTranslator(Translator* translator) 
-{
-	try {
-		translator->addLanguage(Translator::LANG_EN, "en.lang");
-	} catch (const exception& e) {
-		MessageBoxA(nullptr, e.what(), nullptr, 0);
-	}
-
-	try {
-		translator->addLanguage(Translator::LANG_FR, "fr.lang");
-	} catch (const exception& e) {
-		MessageBoxA(nullptr, e.what(), nullptr, 0);
-	}
-
-	return (translator->countLanguages() > 0);
-}
-
-bool initProgram()
-{
-	// TODO: Init translator
-	// TODO: Init logger
-	// TODO: Init ai
-	// TODO: Init server
-	return false;
-}
+bool initProgram(unique_ptr<AIConfig> aiConfig, unique_ptr<ServerConfig> serverConfig);
+bool initTranslator(Translator* translator, string& message);
 
 int main()
 {
-	auto translator = Translator::getInstance();
-	if (!initTranslator(translator)) {
-		MessageBoxA(nullptr, "No language file available.\nProgram will shutdown.", nullptr, 0);
+	unique_ptr<AIConfig> aiConfig = nullptr;
+	unique_ptr<ServerConfig> serverConfig = nullptr;
+	if (!initProgram(move(aiConfig), move(serverConfig))) {
 		return EXIT_FAILURE;
 	}
 	
-	// Init config for the AI
-	auto aiConfig = unique_ptr<AIConfig>(new AIConfig("ai.conf"));
-	translator->setLanguage(aiConfig->Lang);
-
 	// Create the AI
 	auto artificialIntelligence = unique_ptr<AI>(new AI(move(aiConfig)));
 	
-	// Init config for the server
-	auto serverConfig = unique_ptr<ServerConfig>(new ServerConfig("server.conf"));
-
 	// Start the server on the AI
 	artificialIntelligence->StartConnection(move(serverConfig));
 	
 	Translator::freeInstance();
     return EXIT_SUCCESS;
+}
+
+bool initProgram(unique_ptr<AIConfig> aiConfig, unique_ptr<ServerConfig> serverConfig)
+{
+	bool programInitialized = true;
+	string message = "";
+
+	// TODO: Init logger
+	
+	// Init config for the AI
+	bool aiInitialized = false;
+	try {
+		aiConfig = unique_ptr<AIConfig>(new AIConfig("ai.conf"));
+		aiInitialized = true;
+	} catch (const exception& e) {
+		message += e.what();
+		message += "\n";
+	}
+	programInitialized &= aiInitialized;
+
+	// Init translator
+	auto translator = Translator::getInstance();
+	bool translatorInitialized = initTranslator(translator, message);
+	if (translatorInitialized && aiInitialized) {
+		translator->setLanguage(aiConfig->Lang);
+	}
+	programInitialized &= translatorInitialized;
+
+	// Init config for the server
+	bool serverInitialized = false;
+	try {
+		serverConfig = unique_ptr<ServerConfig>(new ServerConfig("server.conf"));
+		serverInitialized = true;
+	} catch (const exception& e) {
+		message += e.what();
+		message += "\n";
+	}
+	programInitialized &= serverInitialized;
+
+	// Check program initializations
+	if (!programInitialized) {
+		MessageBoxA(nullptr, message.c_str(), nullptr, 0);
+	}
+	return programInitialized;
+}
+
+bool initTranslator(Translator* translator, string& message) 
+{
+	try {
+		translator->addLanguage(Translator::LANG_EN, "en.lang");
+	} catch (const exception& e) {
+		message += e.what();
+		message += "\n";
+	}
+
+	try {
+		translator->addLanguage(Translator::LANG_FR, "fr.lang");
+	} catch (const exception& e) {
+		message += e.what();
+		message += "\n";
+	}
+
+	const auto isInitialized = translator->isInitialized();
+	if (!isInitialized) {
+		message += "Translator is not initialized : No language file available.\n";
+	}
+
+	return isInitialized;
 }
