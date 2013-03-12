66d3be3f3f59635bc8120091cfa6976653a3c2b9
Cyril Leroux <cyril.leroux@cgi.com>
2013-03-12 17:27:40 +0100

    Added TODO comments in voice_recognition.h (API sub-project)

--- CppAPI/src/voice_recognition.h
+++ CppAPI/src/voice_recognition.h
@@ -5,5 +5,8 @@
 namespace voice_recognition
 {
 	//void listen_to(const std::string& key_word, const std::string& sentence, bool (*action)());
+	// TODO: Create a VoiceRecognitionEntity
+	// TODO: Create an VocalCommand that could be associate to an action (callback)
+	// TODO: externalize UremoteServer VoiceListener stuffs here
 }
 
