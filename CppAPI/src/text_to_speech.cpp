3a28696d4f37995aefc06b64fa68a1092eef383f
Cyril Leroux <cyril.leroux@cgi.com>
2013-03-15 15:25:35 +0100

    Defined function text_to_speech::langToAttribute and text_to_speech::genderToAttribute. Added text_to_speech::language namespace and text_to_speech::default_lang constant.

--- CppAPI/src/text_to_speech.cpp
+++ CppAPI/src/text_to_speech.cpp
@@ -7,38 +7,18 @@
 #include "Utils.h"
 #include "string_utils.h"
 
-//BOOL CALLBACK EnumUILanguagesProc(LPTSTR wLanguage, LONG_PTR lParam);
-//
-//static std::vector<std::string> languages;
-//
-//// TODO: clean function text_to_speech::available_languages()
-//std::vector<std::string> text_to_speech::available_languages()
-//{
-//	languages = std::vector<std::string>();
-//	if(FAILED(EnumUILanguages(EnumUILanguagesProc, MUI_LANGUAGE_NAME | MUI_USE_SEARCH_ALL_LANGUAGES, 0))) {
-//		std::cout << GetLastError() << std::endl;
-//	}
-//	return languages;
-//}
-//
-//BOOL CALLBACK EnumUILanguagesProc(LPTSTR  wLanguage, LONG_PTR lParam)
-//{
-//	const auto language = string_utils::wstring_to_string(std::wstring(wLanguage));
-//	std::cout << "Langues : " << language << std::endl;
-//	languages.push_back(language);
-//	return TRUE;
-//}
-
 std::vector<std::string> text_to_speech::available_languages()
 {
+	// TODO: Find a way to implement text_to_speech::available_languages()
 	return std::vector<std::string>();
 }
 
 /**
 * Try to initialize a voice object to check if the properties are OK.
+* @return True if parameters are correct, false othewise.
 * @throws and Exception if a problem occurs.
 */
-void text_to_speech::testParameters(const std::string& language, const std::string& gender)
+bool text_to_speech::testParameters(const std::string& language, const std::string& gender)
 {
 	// Init COM lib
 	ComHandler comHandler(*Utils::getLogger());
@@ -46,22 +26,52 @@ void text_to_speech::testParameters(const std::string& language, const std::stri
 	ISpVoice * ispVoice = nullptr;
 	hr = ::CoCreateInstance(CLSID_SpVoice, nullptr, CLSCTX_ALL, IID_ISpVoice, reinterpret_cast<void**>(&ispVoice));
 	ComHelper::checkResult("Speech::testParameters", hr);
-	initVoice(ispVoice, language, gender);
+
+	try {
+		initVoice(ispVoice, language, gender);
+		return true;
+	} catch (const Exception&) {
+		return false;
+	}
 }
 
 void text_to_speech::initVoice(ISpVoice * ispVoice, const std::string& language, const std::string& gender)
 {
-	// TODO: initialize Language and gender in a map
-	const wchar_t* reqAttributsDefault = L"Language=409";
-	const wchar_t* reqAttributs = (language == "FR")	? L"Language=40C" : reqAttributsDefault;
-	const wchar_t* optAttributs = (gender == "M")		? L"Gender=Male"  : L"Gender=Female";
+	const wchar_t* reqAttributs = langToAttribute(language);
+	const wchar_t* optAttributs = genderToAttribute(gender);
 
 	ISpObjectToken* cpTokenEng;
 	HRESULT hr = ::SpFindBestToken(SPCAT_VOICES, reqAttributs, optAttributs, &cpTokenEng);
 	if (FAILED(hr)) {
+		// TODO: create and use a SoftwareException here 
 		throw Exception("SoftwareException", "Speech::initVoice", "Couldn't find a Token with the required attributs.");
 	}
 	ispVoice->SetVoice(cpTokenEng);
 	//TODO: Config Rate with file
 	ispVoice->SetRate(long(0.5));
+}
+
+const wchar_t* text_to_speech::langToAttribute(std::string language)
+{
+	// TODO: initialize Language in a map
+	// Default value : 409 = English 
+	const wchar_t* defaultValue = L"Language=409";
+
+	if (language == "FR") {
+		return L"Language=40C";
+	}
+
+	return defaultValue;
+}
+
+// TODO: initialize Gender in a map
+const wchar_t* text_to_speech::genderToAttribute(std::string gender)
+{
+	const wchar_t* defaultValue = L"Gender=Female";
+
+	if (gender == "M") {
+		return L"Gender=Male";
+	}
+
+	return defaultValue;
 }
--- CppAPI/src/text_to_speech.h
+++ CppAPI/src/text_to_speech.h
@@ -7,8 +7,19 @@
 
 namespace text_to_speech
 {
+	namespace languague
+	{
+		const std::string EN = "EN";
+		const std::string FR = "FR";
+	};
+
+	const std::string default_lang = languague::EN;
+
 	std::vector<std::string> available_languages();
-	void testParameters(const std::string& language, const std::string& gender);
+	bool testParameters(const std::string& language, const std::string& gender);
 	void initVoice(ISpVoice * ispVoice, const std::string& language, const std::string& gender);
+
+	const wchar_t* langToAttribute(std::string language);
+	const wchar_t* genderToAttribute(std::string gender);
 };
 
